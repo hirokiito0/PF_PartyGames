22ef00b2d7b727aa4f945478b60e9525896ee159
hirokiito0 <syu.itou.h@gamil.com>
2021-08-14 11:36:29 +0000

    いいね機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :games,    dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :games,     dependent: :destroy
+  has_many :comments,  dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   attachment :image
 end
