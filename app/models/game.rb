1f02d845f59f3d82b02a85f83c530a7075a2007c
hirokiito0 <syu.itou.h@gmail.com>
2021-08-22 12:38:56 +0000

    ソート機能追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,9 +3,26 @@ class Game < ApplicationRecord
 #  belongs_to :admin
   has_many :comments,  dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   attachment :game_image
-  
+
+  def change
+    create_table :games do |t|
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+
+  ransacker :favorites_count do
+    query = '(SELECT COUNT(favorites.game_id) FROM favorites where favorites.game_id = games.id GROUP BY favorites.game_id)'
+    Arel.sql(query)
+  end
+
+  ransacker :rates_count do
+    query = '(SELECT (comments.rate) FROM comments where comments.game_id = games.id )'
+    Arel.sql(query)
+  end
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
